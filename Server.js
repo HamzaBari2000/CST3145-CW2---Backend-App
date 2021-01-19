447b0a93f773022a14ebd262b8fec4765a4a5503
HamzaBari2000 <67334675+HamzaBari2000@users.noreply.github.com>
2021-01-19 22:25:18 +0000

    commit 18

--- Server.js
+++ Server.js
@@ -51,6 +51,8 @@ app.use(function (req, res, next) {
 app.post("/collection/:collectionName", (req, res, next) => {
   req.collection.insert(req.body, (e, results) => {
     if (e) return next(e);
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
     res.send(results.ops);
   });
 });

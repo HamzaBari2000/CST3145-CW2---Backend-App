8c8314db03c3e5352c085d4cc548ddaaf82accad
HamzaBari2000 <67334675+HamzaBari2000@users.noreply.github.com>
2021-01-19 18:57:04 +0000

    commit 13

--- Server.js
+++ Server.js
@@ -30,6 +30,7 @@ app.get("/", (req, res, next) => {
 app.get("/collection/:collectionName", (req, res, next) => {
   req.collection.find({}).toArray((e, results) => {
     if (e) return next(e);
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.send(results);
   });
 });

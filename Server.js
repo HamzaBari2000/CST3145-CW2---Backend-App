ec1b60a49bfe2a5a562c32d1ed9359fc45d2fd8b
HamzaBari2000 <67334675+HamzaBari2000@users.noreply.github.com>
2021-01-20 00:08:31 +0000

    commit 19 - test

--- Server.js
+++ Server.js
@@ -17,14 +17,6 @@ MongoClient.connect(
   }
 );
 
-app.use(function (req, res, next) {
-  // allow different IP address
-  res.header("Access-Control-Allow-Origin", "*");
-  // allow different header fields
-  res.header("Access-Control-Allow-Headers", "*");
-  next();
-});
-
 app.param("collectionName", (req, res, next, collectionName) => {
   req.collection = db.collection(collectionName);
   return next();
@@ -52,6 +44,8 @@ app.post("/collection/:collectionName", (req, res, next) => {
   req.collection.insert(req.body, (e, results) => {
     if (e) return next(e);
     res.send(results.ops);
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
   });
 });
 

fc5ed5c6adcc1b0ad2032cfc6ecf412f64289caa
HamzaBari2000 <67334675+HamzaBari2000@users.noreply.github.com>
2021-01-20 00:35:55 +0000

    commit 24 - another test

--- Server.js
+++ Server.js
@@ -7,11 +7,31 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+// Add headers
 app.use(function (req, res, next) {
-  // allow different IP address
-  res.header("Access-Control-Allow-Origin", "*");
-  // allow different header fields
-  res.header("Access-Control-Allow-Headers", "*");
+  // Website you wish to allow to connect
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://cst3145--cw2.herokuapp.com"
+  );
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
   next();
 });
 

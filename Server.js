f899a7e33ea6dcd00b562ba943338160058f2718
HamzaBari2000 <67334675+HamzaBari2000@users.noreply.github.com>
2021-01-20 00:41:34 +0000

    commit 26

--- Server.js
+++ Server.js
@@ -39,36 +39,9 @@ app.get("/collection/:collectionName", (req, res, next) => {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Add headers
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://cst3145--cw2.herokuapp.com"
-  );
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
 //A POST Route Which saves the new Order to the Database.
 app.post("/collection/:collectionName", (req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
   req.collection.insert(req.body, (e, results) => {
     if (e) return next(e);
     res.send(results.ops);

2b66d5e18db41baf266fc95a8acae2ae0fd9737e
HamzaBari2000 <67334675+HamzaBari2000@users.noreply.github.com>
2021-01-26 02:59:51 +0000

    commit 36

--- Server.js
+++ Server.js
@@ -7,18 +7,6 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function (req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  ); // If needed
-  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,contenttype"); // If needed
-  res.setHeader("Access-Control-Allow-Credentials", true); // If needed
-
-  res.send("cors problem fixed:)");
-});
-
 //Start of the database
 
 let db;
@@ -53,8 +41,19 @@ app.use(bodyParser.json());
 app.post("/collection/:collectionName", (req, res, next) => {
   req.collection.insert(req.body, (e, results) => {
     if (e) return next(e);
+    res.header("Access-Control-Allow-Origin", "*");
+    // allow different header fields
+    res.header("Access-Control-Allow-Headers", "*");
     res.send(results.ops);
   });
+});
+
+app.use(function (req, res, next) {
+  // allow different IP address
+  res.header("Access-Control-Allow-Origin", "*");
+  // allow different header fields
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
 });
 
 //A PUT Route which updates the number of available spaces in the ‘lesson’ collection.

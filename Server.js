a0f9d8cd25e1e1863e6a1f756086d71f92e2634c
HamzaBari2000 <67334675+HamzaBari2000@users.noreply.github.com>
2021-01-26 03:09:00 +0000

    commit 37

--- Server.js
+++ Server.js
@@ -23,8 +23,9 @@ app.param("collectionName", (req, res, next, collectionName) => {
 });
 
 app.get("/", (req, res, next) => {
-  res.send("Begginning");
-  res.send("Select a collection, e.g., /collection/messages");
+  res.send(
+    "Select a collection, e.g., /collection/collectionName\nSelect a Image, e.g., /Img/ImageName.png"
+  );
 });
 
 //A GET Route which returns all the lessons.
